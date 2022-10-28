2c4b22e4199e951be9b4bffdd7d6ad048dcee65d
buwuzhengyecc <786174010@qq.com>
2022-10-28 23:51:27 +0800

    第一次尝试

---
+++ FristTry.py
@@ -0,0 +1,9 @@
+from subprocess import _TXT
+import time
+
+print("---RUNOOB EXAMPLE ： Loading 效果---")
+
+print("Loading", end="")
+for i in range(20):
+    print(".", end="",flush=True,file=F:\test.txt)
+    time.sleep(0.5)
